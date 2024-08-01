a32398d19c580d9c47bfb23876e796d29524eee4
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-08-01 14:10:18 +0530

    Backend validation using Joi

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require('./utils/wrapAsync.js');
 const ExpressError = require('./utils/ExpressError.js');
+const { listingSchema } = require('./schema.js');
 
 const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
 main().then(()=>{console.log("Connected to DB");}).catch((err)=>{console.log(err)});
@@ -22,6 +23,15 @@ app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const validateListing = (req,res,next) =>{
+    let {error} = listingSchema.validate(req.body);
+    if (result.error) {
+        let errMsg = error.details.map((el) =>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }
+    else next();
+}
+
 //index
 app.get("/listings", wrapAsync(async (req,res)=>{
     const allListings= await Listing.find({});
@@ -34,12 +44,12 @@ app.get("/listings/new",(req,res)=>{
  })
  
 //  //Create Route
-app.post("/listings", 
+app.post("/listings", validateListing,
    wrapAsync(async (req, res) => {
-    if (req.body.listing) {
-        throw new ExpressError(400,"Send valid data for listing");
-    }
+   
    const { listing, imageurl } = req.body;
+   console.log(req.body);
+   
    const newListing = new Listing({
        ...listing,
        image: { url: imageurl }
@@ -49,7 +59,7 @@ app.post("/listings",
     console.log(listing);
 })
 ); 
-    
+        
    
  //Edit Route
  app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
@@ -58,11 +68,8 @@ app.post("/listings",
     res.render("listings/edit.ejs",{listing});
  }))
  //Update Route
- app.put("/listings/:id",wrapAsync(async (req,res)=>{
+ app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>{
     let {id} =req.params;
-    if (req.body.listing) {
-        throw new ExpressError(400,"Send valid data for listing");
-    }
    await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`); 
  }));
@@ -93,7 +100,8 @@ app.all('*',(req,res,next)=>{
 
 app.use((err,req,res,next)=>{
     let {statusCode=500 , message} = err;
-    res.status(statusCode).send(message);
+    // res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs",{message});
 })
 
 app.listen(8080 , ()=>{
